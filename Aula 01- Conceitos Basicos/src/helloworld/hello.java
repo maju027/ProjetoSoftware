04caaaa5f726cdb1b75d268f46fa0ee8f935a14d
maju027 <julyamarinho027@outlook.com>
2025-02-12 10:45:08 -0300

    .

---
+++ Aula 01- Conceitos Basicos/src/helloworld/hello.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.print("Hello World");       
+		System.out.print("Hello World");       
+
+	}
+
+}
